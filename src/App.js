c44bd1c6adb9ddb6d8bd3c6ec29408312a3b5621
James Dale <git@jamesdale.com.au>
2018-05-06 04:32:05 +1000

    Update login, login page, firebase/auth and mobile site

--- src/App.js
+++ src/App.js
@@ -11,14 +11,27 @@ import NavigationBar from './components/NavigationBar.js'
 import LoginPage from './pages/LoginPage.js'
 import ChallengesPage from './pages/ChallengesPage.js'
 import CodePage from './pages/CodePage.js'
+import { BrowserView, MobileView, isBrowser, isMobile } from "react-device-detect";
+
+import ArmChair from './images/armchair.svg'
 
 const App = () => (
   <Router>
     <div>
       <NavigationBar></NavigationBar>
-      <Route exact path="/" component={ChallengesPage}/>
-      <Route path="/login" component={LoginPage}/>
-      <Route path="/code" component={CodePage}/>
+      <BrowserView device={false}>
+        <Route exact path="/" component={ChallengesPage}/>
+        <Route path="/login" component={LoginPage}/>
+        <Route path="/code" component={CodePage}/>
+      </BrowserView>
+      <MobileView device={true}>
+        <div className="mobileWarning">
+          <img src={ArmChair}/>
+          <h1> Coding is more comfortable on your laptop. </h1>
+        </div>
+      </MobileView>
+
+
     </div>
   </Router>
 )
---
+++ src/Voice.js
@@ -0,0 +1,12 @@
+
+
+class Voice {
+
+		constructor() {
+
+
+
+		}
+}
+
+export default Voice;
