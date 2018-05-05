ed817b7bc58d753ee174b63fef483fc8b091b8d8
James Dale <git@jamesdale.com.au>
2018-05-06 08:15:21 +1000

    Update challenge box to be more personalised

--- src/components/ChallengeBox.js
+++ src/components/ChallengeBox.js
@@ -15,7 +15,7 @@ class ChallengeBox extends Component {
               <div className="challengeBox">
                   <Flex p={3} align='center'>
                       <Box w={1/12}><img alt="User" src={UserImage}></img></Box>
-                      <Box w={10/12}>Angela is completing {this.props.name}</Box>
+                      <Box w={10/12}>{localStorage.getItem('displayName').split(" ")[0]}, you can compete in {this.props.name}</Box>
                       <Box w={1/12}><Link to={{"pathname":"/code/"+this.props.problemID, "state":{"problemDescription":this.props.problemDescription} }} className="btn-join">Join</Link></Box>
                   </Flex>
               </div>
