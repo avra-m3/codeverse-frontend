560eea8ee87da6b2bbd5a087a87ec3a34aff5612
Wilson Mun <wilsonmungit@gmail.com>
2018-05-05 21:15:49 +1000

    imported font-awesome and initial LoginPage.js and LoginPage.css

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
+import { render } from 'react-dom';
+
 import axios from 'axios'
 
+import { Flex, Box } from 'reflexbox';
+import '../LoginPage.css'
+import '../css/fontawesome-all.min.css'
 
+import UserImage from '../images/angela.png';
+import CodePanel from '../components/CodePanel.js';
+// import LoadingButton from '../components/LoadingButton'
 
 class LoginPage extends Component {
 
@@ -15,7 +23,28 @@ class LoginPage extends Component {
   render() {
     return (
       <div className="App">
-        
+          <div className="container">
+            <Flex p={2} align='left'>
+                <Box className="left-box" px={2} w={1/2}>
+                    <p align="center">CodeVerse</p>
+                </Box>
+                <Box className="right-box" px={2} w={1/2}>
+                    <Box className="right-top-box" px={2} w={1}>
+                        <p align="center"><i className="fab fa-facebook-square"></i>Sign in via Facebook</p>
+                        <p class="btn-join" align="center">Continue as Bob Tom</p>
+                        {/*<LoadingButton className={""}>rf31r31r31r31</LoadingButton>*/}
+                    </Box>
+                    <Box className="right-bottom-box" px={2} w={1}>
+                        <form onSubmit={this.handleSubmit}>
+                            <p align="center"><textarea>test</textarea></p>
+                            <p align="center"><textarea>test1</textarea></p>
+                            <p align="center"><input class="btn-join" type="submit" value="submit"/></p>
+                        </form>
+                    </Box>
+                </Box>
+
+            </Flex>
+          </div>
       </div>
     );
   }
