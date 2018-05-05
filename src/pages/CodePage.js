96d18bc6b50ab0c21717da5111ef8b641ef7a718
James Dale <git@jamesdale.com.au>
2018-05-06 07:39:55 +1000

    add autoclose

--- src/pages/CodePage.js
+++ src/pages/CodePage.js
@@ -48,7 +48,7 @@ class CodePage extends Component {
 
     return (
       <div className="App">
-      <ToastContainer />
+      <ToastContainer autoClose={4000} />
         <div className="container">
 
 
