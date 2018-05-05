7821ed3e3ca7a966e1a327ec359d712b8392ca19
Wilson Mun <wilsonmungit@gmail.com>
2018-05-06 01:07:42 +1000

    added youtube video and randomizer in challengebox.js

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -30,17 +30,19 @@ class LoginPage extends Component {
             <Flex p={2} align='left'>
                 <Box className="left-box" px={2} w={1/2}>
                     <p align="center">CodeVerse</p>
+                    <iframe width="100%" height="80%" src="https://www.youtube.com/embed/evef8FCK6wk?autoplay=1&iv_load_policy=3" frameBorder="0"
+                            allow="autoplay; encrypted-media" allowFullScreen></iframe>
                 </Box>
                 <Box className="right-box" px={2} w={1/2}>
                     <Box className="right-top-box" px={2} w={1}>
-                        <p align="center"><i className="fab fa-facebook-square"></i>Sign in via Facebook</p>
-                        <p class="btn-join" align="center"><img src={FBLoginBtn} width="200vw"/></p>
+                        {/*<p align="center"><i className="fab fa-facebook-square"></i>Sign in via Facebook</p>*/}
+                        <p align="center"><img src={FBLoginBtn} width="400vw"/></p>
                         {/*<LoadingButton className={""}>rf31r31r31r31</LoadingButton>*/}
                     </Box>
                     <Box className="right-bottom-box" px={2} w={1}>
                         <form onSubmit={this.handleSubmit}>
-                            <p align="center"><input type="text"/></p>
-                            <p align="center"><input type="text"/></p>
+                            <p align="center"><label>Login: <input type="text"/></label></p>
+                            <p align="center"><label>Password: <input type="text"/></label></p>
                             <p align="center"><input class="btn-join" type="submit" value="submit"/></p>
                         </form>
                     </Box>
